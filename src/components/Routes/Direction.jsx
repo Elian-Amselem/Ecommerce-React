59782226082cd041704e1ad8ed03f9e159474d32
Franco Ruiz <francojruiz10@gmail.com>
2023-09-25 23:49:36 -0300

    estilos puestos

--- src/components/Routes/Direction.jsx
+++ src/components/Routes/Direction.jsx
@@ -1,7 +1,10 @@
+Aa
 import { BrowserRouter as Router,Route,Routes,Link } from 'react-router-dom'
-import Contacto from '../Contacto/Contacto'
+import Contacto from
+ '../Contacto/Contacto'
 import NotFound from '../NotFound/NotFound'
 import Home from '../Home/Home'
+
 function Direction() {
 return (
     // Enrutamiento
@@ -24,4 +27,5 @@ return (
     </Router>
 )
 }
+
 export default Direction;
